bfca221357f944ec364f7c9ac57971773dc3c49d
Felipe Mamede <felipebrasilmv@hotmail.com>
2021-03-09 21:24:27 -0300

    Finalizando parte 01 do modulo

--- chapter-1/01-github-explorer/webpack.config.js
+++ chapter-1/01-github-explorer/webpack.config.js
@@ -1,8 +1,11 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const isDevelopment = process.env.NODE_ENV !== 'production'
+
 module.exports = {
-  mode: 'development',
+  mode: isDevelopment ? 'development' : 'production',
+  devtool: isDevelopment ? 'eval-source-map' : 'source-map',
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -16,12 +19,20 @@ module.exports = {
       template: path.resolve(__dirname, 'public', 'index.html')
     })
   ],
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  },
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: 'babel-loader',
+      },
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       }
     ],
   }
