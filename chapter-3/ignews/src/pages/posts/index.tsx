025e5dcb55d76f7cb63992bb0b4ad916f8e5b670
Felipe Mamede <felipebrasilmv@hotmail.com>
2021-04-19 00:17:35 -0300

    Trabalhando com CMS finalizado

--- chapter-3/ignews/src/pages/posts/index.tsx
+++ chapter-3/ignews/src/pages/posts/index.tsx
@@ -1,27 +1,43 @@
 import { GetStaticProps } from 'next';
-import Head from 'next/head'
 import Prismic from '@prismicio/client'
-
+import Head from 'next/head'
 import { getPrismicClient } from '../../services/prismic';
+import { RichText } from 'prismic-dom'
 
 import styles from './styles.module.scss'
+import Link from 'next/link';
 
-export default function Posts() {
+type Post = {
+  slug: string;
+  title: string;
+  except: string;
+  updatedAt: string;
+}
+
+interface PostProps {
+  posts: Post[];
+}
+
+export default function Posts({posts}: PostProps) {
   return (
     <>
-    <Head>
-      <title>Posts | Ignews </title>
-    </Head>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
 
-    <main className={styles.container}>
-      <div className={styles.posts}>
-        <a href="#">
-          <time>12 de março de 2021</time>
-          <strong>Creating a Monorepo with Learn & Yarn Workspaces</strong>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam malesuada rutrum leo, eget volutpat metus sodales vitae.</p>
-        </a>
-      </div>
-    </main>
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          {posts.map(post => (
+            <Link href={`/posts/${post.slug}`} key={post.slug}>
+              <a >
+                <time>{post.updatedAt}</time>
+                <strong>{post.title}</strong>
+                <p>{post.except}</p>
+              </a>
+            </Link>
+          )) }
+          </div>
+      </main>
     </>
   );
 }
@@ -30,15 +46,28 @@ export const getStaticProps: GetStaticProps = async () => {
   const prismic = getPrismicClient()
 
   const response = await prismic.query([
-    Prismic.predicates.at('document.type', 'publication')
+    Prismic.Predicates.at('document.type', 'post')
   ], {
-    fetch: ['publication.title', 'publication.content'],
+    fetch: ['post.title', 'post.content'],
     pageSize: 100,
   })
 
-  console.log(JSON.stringify(response, null, 2))
+  const posts = response.results.map(post => {
+    return {
+      slug:post.uid,
+      title: RichText.asText(post.data.title),
+      except: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
+      updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
+        day: '2-digit',
+        month: 'long',
+        year: 'numeric'
+      })
+    }
+  })
 
   return {
-    props: {}
+    props: {
+      posts
+    }
   }
 }
